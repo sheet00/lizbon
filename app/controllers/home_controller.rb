9509636e731a7b217f844509a63a53495322dd07
sheet00 <sheet00@rd5.so-net.ne.jp>
2017-12-11 22:10:29 +0900

    [update] trade 売買実行順序の変更 #4

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,7 +4,7 @@ class HomeController < ApplicationController
     @wallet_histories = WalletHistory.order("id desc").take(15)
     @active_orders = ActiveOrder.order("id desc").take(10)
     @trade_histories = TradeHistory.order("timestamp desc").take(100)
-    @bots = Bot.order("id desc").take(100)
+    @bots = Bot.order("id desc").take(500)
     @t_settings = TradeSetting.all
   end
 end
