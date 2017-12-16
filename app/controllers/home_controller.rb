541fa1ae3d0ab388db86cdcfabf367f54c014ee7
sheet00 <sheet00@rd5.so-net.ne.jp>
2017-12-16 16:35:38 +0900

    [update] HOME画面で対象通貨の確認 #22

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,8 +3,9 @@ class HomeController < ApplicationController
     @wallets = Wallet.exclude_order
     @wallet_histories = WalletHistory.order("trade_time desc").take(15)
     @active_orders = ActiveOrder.order("id desc").take(10)
-    @trade_histories = TradeHistory.order("timestamp desc").take(100)
+    @trade_histories = TradeHistory.order("timestamp desc, id desc").take(100)
     @bots = Bot.order("id desc").take(500)
+    @targets = Target.all
     @t_settings = TradeSetting.all
   end
 end
