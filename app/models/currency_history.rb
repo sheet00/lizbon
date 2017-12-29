403633cdb82b46e2fe02da1ff260df87047d0cd6
sheet00 <sheet00@rd5.so-net.ne.jp>
2017-12-29 12:52:25 +0900

    [change]ログ削除 ロスカット価格の算出基準に合わせて削除するよう修正

--- app/models/currency_history.rb
+++ app/models/currency_history.rb
@@ -2,7 +2,7 @@ class CurrencyHistory < ApplicationRecord
 
   # 実行日からn時間前のログ削除
   def self.delete_before_log
-    average_min = TradeSetting.where(trade_type: :average_list_min).first.value
+    average_min = TradeSetting.where(trade_type: :sell_lower_min).first.value
     to = Time.now - average_min.minute
     self.where("created_at < ?", to.to_s).delete_all
   end
