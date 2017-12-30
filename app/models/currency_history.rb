39fbeab75d119f790b60fe5ec4379964051c63cc
sheet00 <sheet00@rd5.so-net.ne.jp>
2017-12-30 12:56:06 +0900

    [change] delete_bofore_log 削除時間の設定方法修正

--- app/models/currency_history.rb
+++ app/models/currency_history.rb
@@ -2,7 +2,14 @@ class CurrencyHistory < ApplicationRecord
 
   # 実行日からn時間前のログ削除
   def self.delete_before_log
-    average_min = TradeSetting.where(trade_type: :sell_lower_min).first.value
+    #売り下限時間、移動平均確認時間、両者のうち、長いほうに合わせて削除する
+    average_min = TradeSetting
+    .where(trade_type: :sell_lower_min)
+    .or(TradeSetting.where(trade_type: :average_list_min))
+    .maximum(:value)
+
+    ap average_min
+
     to = Time.now - average_min.minute
     self.where("created_at < ?", to.to_s).delete_all
   end
