bf66d900bc2d141ae5b1d126cec1beece8940289
sheet00 <sheet00@rd5.so-net.ne.jp>
2018-01-04 12:16:27 +0900

    [change] delay jobリトライの追加

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -1,6 +1,6 @@
 # 失敗したジョブをキュー削除管理
 Delayed::Worker.destroy_failed_jobs = false
 # リトライ実行
-Delayed::Worker.max_attempts = 0
+Delayed::Worker.max_attempts = 10
 # delayjob log
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'job.log'))
